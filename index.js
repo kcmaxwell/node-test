555c5451962b39b60bcf00fffb0237275bd5bc32
Kristopher Maxwell <zerotrig@gmail.com>
2022-02-03 13:28:19 -0700

    Created basic HTML pages, serve them with node

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
 const http = require('http')
+const fs = require('fs')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+
+
+    if (req.url == '/about.html') {
+        res.writeHead(200, { 'content-type': 'text/html'})
+        fs.createReadStream('about.html').pipe(res);
+    } else if (req.url == '/contact-me.html') {
+        res.writeHead(200, { 'content-type': 'text/html'})
+        fs.createReadStream('contact-me.html').pipe(res);
+    } else if (req.url == '/') {
+        res.writeHead(200, { 'content-type': 'text/html'})
+        fs.createReadStream('index.html').pipe(res);
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html'})
+        fs.createReadStream('404.html').pipe(res);
+    }
 })
 
 server.listen(port, () => {
